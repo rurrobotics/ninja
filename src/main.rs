376a24c3be5d05824a1007044588aa67f4406b7d
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-03-07 17:23:38 +0100

    feat: usb logging

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,9 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_rp::gpio;
+use embassy_rp::{peripherals::USB, usb};
 use embassy_time::Timer;
-use gpio::{Level, Output};
-use {panic_halt as _};
+use panic_halt as _;
 
 #[unsafe(link_section = ".bi_entries")]
 #[used]
@@ -16,16 +15,28 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
+embassy_rp::bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => usb::InterruptHandler<USB>;
+});
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let mut led = Output::new(p.PIN_25, Level::Low);
 
+    spawner.must_spawn(logger_task(p.USB));
+
+    let mut i: u8 = 0;
     loop {
-        led.set_high();
-        Timer::after_millis(250).await;
+        i = i.wrapping_add(1);
+        log::info!("USB says: {}", i);
 
-        led.set_low();
-        Timer::after_millis(250).await;
+        Timer::after_secs(1).await;
     }
+}
+
+#[embassy_executor::task]
+async fn logger_task(usb: embassy_rp::Peri<'static, embassy_rp::peripherals::USB>) {
+    let driver = embassy_rp::usb::Driver::new(usb, Irqs);
+
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
 }
