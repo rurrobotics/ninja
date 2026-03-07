2497155082ee78409cd1b72ccd9c17500b30ff14
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-03-07 20:11:20 +0100

    feat: add wifi init

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #![no_std]
 #![no_main]
 
+mod wifi;
+
 use core::ffi::CStr;
 
+use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
 use embassy_executor::Spawner;
-use embassy_rp::{peripherals::USB, usb};
-use embassy_time::Timer;
+use embassy_rp::{
+    bind_interrupts,
+    gpio::{Level, Output},
+    peripherals::{DMA_CH0, PIO0, USB},
+    pio::{self, Pio}, usb,
+};
+use embassy_time::{Duration, Timer};
 use panic_halt as _;
+use static_cell::StaticCell;
 
 const NAME: &CStr = unsafe {
     CStr::from_bytes_with_nul_unchecked(concat!(env!("CARGO_PKG_NAME"), "\0").as_bytes())
@@ -25,15 +34,51 @@ pub static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 4] = [
     embassy_rp::binary_info::rp_program_build_attribute!(),
 ];
 
-embassy_rp::bind_interrupts!(struct Irqs {
+bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => usb::InterruptHandler<USB>;
+    PIO0_IRQ_0 => pio::InterruptHandler<PIO0>;
 });
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
+    let fw = include_bytes!("../firmware/43439A0.bin");
+    let clm = include_bytes!("../firmware/43439A0_clm.bin");
 
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        // SPI communication won't work if the speed is too high, so we use a divider larger than `DEFAULT_CLOCK_DIVIDER`.
+        // See: https://github.com/embassy-rs/embassy/issues/3960.
+        RM2_CLOCK_DIVIDER,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        p.DMA_CH0,
+    );
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let state = STATE.init(cyw43::State::new());
+    let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
+    spawner.must_spawn(cyw43_task(runner));
     spawner.must_spawn(logger_task(p.USB));
+
+    control.init(clm).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::PowerSave)
+        .await;
+
+}
+
+#[embassy_executor::task]
+async fn cyw43_task(
+    runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>,
+) -> ! {
+    runner.run().await
 }
 
 #[embassy_executor::task]
