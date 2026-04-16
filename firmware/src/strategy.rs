a6273aa69626398ad96fc690954e0003f01e2f5e
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2026-04-17 01:42:20 +0200

    feat: strategy hardcoded

--- firmware/src/strategy.rs
+++ firmware/src/strategy.rs
@@ -1,5 +1,4 @@
-use embassy_futures::join::join;
-use embassy_time::{Duration, Instant, Timer};
+use embassy_time::Timer;
 
 use crate::tasks::{DrivetrainType, EnablesType, GripperType};
 
@@ -8,46 +7,25 @@ pub async fn handle_game<'d>(
     drivetrain: &mut DrivetrainType<'d>,
     enables: &mut EnablesType<'d>,
 ) {
-    let start = Instant::now();
+    enables.0.set_low();
+    enables.1.set_low();
+
+    Timer::after_secs(85).await;
 
     enables.0.set_high();
     enables.1.set_high();
 
-    join(drivetrain.drive(160.0), gripper.open()).await;
-    drivetrain.turn(-90.0).await;
-    drivetrain.drive(180.0).await;
-    gripper.close().await;
-    drivetrain.drive(40.0).await;
-    drivetrain.drive(-210.0).await;
-
-    // Push 1
+    drivetrain.drive(540.0).await;
     drivetrain.turn(90.0).await;
-    drivetrain.drive(240.0).await;
-    drivetrain.turn(-90.0).await;
-    drivetrain.drive(100.0).await;
-    drivetrain.turn(90.0).await;
-    drivetrain.drive(170.0).await;
-    drivetrain.drive(-390.0).await;
-
-    // Leave
-    gripper.open().await;
-    drivetrain.drive(-90.0).await;
-    drivetrain.turn(90.0).await;
-    drivetrain.drive(90.0).await;
-    drivetrain.turn(-90.0).await;
-    drivetrain.drive(480.0).await;
-    drivetrain.drive(-50.0).await;
-    drivetrain.turn(-70.0).await;
-    drivetrain.drive(210.0).await;
+    drivetrain.drive(1000.0).await;
+    drivetrain.drive(-200.0).await;
+    drivetrain.turn(-45.0).await;
+    drivetrain.drive(700.0).await;
 
     enables.0.set_low();
     enables.1.set_low();
 
-    while start.elapsed() < Duration::from_secs(85) {
-        Timer::after_millis(100).await
-    }
-
-    while start.elapsed() < Duration::from_secs(100) {
+    loop {
         gripper.close().await;
         Timer::after_millis(100).await;
         gripper.open().await;
